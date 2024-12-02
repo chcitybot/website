ef7a6b7edb0d38de04ee0fe0237b399d923dbc6a
LuxsSoft <luxs_soft@protonmail.com>
2024-12-02 15:35:35 +0100

    remove tresjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', "@nuxtjs/i18n", '@tresjs/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', "@nuxtjs/i18n"],
   tailwindcss: {
     cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
     configPath: 'tailwind.config',
